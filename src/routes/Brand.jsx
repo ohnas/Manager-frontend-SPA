38058adc80f9a1aee473e1879437ce2ce733380a
ohnas <ohnas12@gmail.com>
2023-01-28 17:40:14 +0900

    11. Brand.jsx

--- src/routes/Brand.jsx
+++ src/routes/Brand.jsx
@@ -1,10 +1,29 @@
-import { baseUrl, getCookie } from "../api";
+import { useEffect, useState } from "react";
+import { baseUrl } from "../api";
 
 
 function Brand() {
+    const [brands, setBrands] = useState([]);
+    async function myBrand() {
+        let response = await fetch(`${baseUrl}/brands/my`, {
+            method : "GET",
+            credentials: "include",
+            headers : {
+                'Content-Type': 'application/json',
+            },
+        });
+        let data = await response.json();
+        setBrands(data);
+    }
+    useEffect(() => {
+        myBrand();
+    }, []);
     return (
-        <div>
-            <span>Brand list</span>
+        <div className="flex flex-col mt-32 justify-center items-center">
+            { brands.map((brand) =>
+                <li key={brand.pk} className="list-none mb-10">{brand.name}</li>
+                )
+            }
         </div>
     );
 }
