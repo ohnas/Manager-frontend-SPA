d95f18bd89c9bd1f8aeb3460da02dd643ab7b607
ohnas <ohnas12@gmail.com>
2023-02-19 18:51:50 +0900

    42. Management.jsx

--- src/routes/Management.jsx
+++ src/routes/Management.jsx
@@ -1,5 +1,5 @@
 import { useEffect } from "react";
-import { useNavigate, useOutletContext } from "react-router-dom";
+import { Link, useNavigate, useOutletContext } from "react-router-dom";
 
 function Management() {
     const { 
@@ -22,28 +22,42 @@ function Management() {
                         <div className="flex flex-col items-center mb-14">
                             <span className="text-2xl mb-5">USER</span>
                             <ul>
-                                <li className="text-gray-400 hover:border-b-2 border-purple-400">update</li>
+                                <Link to={"/management/manageuser/update"}>
+                                    <li className="text-gray-400 hover:border-b-2 border-purple-400">update</li>
+                                </Link>
                             </ul>
                         </div>
                         <div className="flex flex-col items-center mb-14">
                             <span className="text-2xl mb-5">BRAND</span>
                             <ul className="flex justify-between items-center w-32">
-                                <li className="text-gray-400 hover:border-b-2 border-purple-400">create</li>
-                                <li className="text-gray-400 hover:border-b-2 border-purple-400">update</li>
+                                <Link to={"/management/managebrand/create"}>
+                                    <li className="text-gray-400 hover:border-b-2 border-purple-400">create</li>
+                                </Link>
+                                <Link to={"/management/managebrand/update"}>
+                                    <li className="text-gray-400 hover:border-b-2 border-purple-400">update</li>
+                                </Link>
                             </ul>
                         </div>
                         <div className="flex flex-col items-center mb-14">
                             <span className="text-2xl mb-5">PRODUCT</span>
                             <ul className="flex justify-between items-center w-32">
-                                <li className="text-gray-400 hover:border-b-2 border-purple-400">create</li>
-                                <li className="text-gray-400 hover:border-b-2 border-purple-400">update</li>
+                                <Link to={"/management/manageproduct/create"}>
+                                    <li className="text-gray-400 hover:border-b-2 border-purple-400">create</li>
+                                </Link>
+                                <Link to={"/management/manageproduct/update"}>
+                                    <li className="text-gray-400 hover:border-b-2 border-purple-400">update</li>
+                                </Link>
                             </ul>
                         </div>
                         <div className="flex flex-col items-center mb-14">
                             <span className="text-2xl mb-5">SITE</span>
                             <ul className="flex justify-between items-center w-32">
-                                <li className="text-gray-400 hover:border-b-2 border-purple-400">create</li>
-                                <li className="text-gray-400 hover:border-b-2 border-purple-400">update</li>
+                                <Link to={"/management/managesite/create"}>
+                                    <li className="text-gray-400 hover:border-b-2 border-purple-400">create</li>
+                                </Link>
+                                <Link to={"/management/managesite/update"}>
+                                    <li className="text-gray-400 hover:border-b-2 border-purple-400">update</li>
+                                </Link>
                             </ul>
                         </div>
                     </div>
