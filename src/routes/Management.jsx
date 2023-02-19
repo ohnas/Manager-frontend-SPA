30443d177d2e5d4fd543a5314ef6bd9134f3c34e
ohnas <ohnas12@gmail.com>
2023-02-19 17:41:46 +0900

    37. Management.jsx

--- src/routes/Management.jsx
+++ src/routes/Management.jsx
@@ -18,9 +18,37 @@ function Management() {
     return (
         <>
             {user.is_staff ? 
-                <div>Hello staff</div>
+                    <div className="flex flex-col justify-center items-center mt-10">
+                        <div className="flex flex-col items-center mb-14">
+                            <span className="text-2xl mb-5">USER</span>
+                            <ul>
+                                <li className="text-gray-400 hover:border-b-2 border-purple-400">update</li>
+                            </ul>
+                        </div>
+                        <div className="flex flex-col items-center mb-14">
+                            <span className="text-2xl mb-5">BRAND</span>
+                            <ul className="flex justify-between items-center w-32">
+                                <li className="text-gray-400 hover:border-b-2 border-purple-400">create</li>
+                                <li className="text-gray-400 hover:border-b-2 border-purple-400">update</li>
+                            </ul>
+                        </div>
+                        <div className="flex flex-col items-center mb-14">
+                            <span className="text-2xl mb-5">PRODUCT</span>
+                            <ul className="flex justify-between items-center w-32">
+                                <li className="text-gray-400 hover:border-b-2 border-purple-400">create</li>
+                                <li className="text-gray-400 hover:border-b-2 border-purple-400">update</li>
+                            </ul>
+                        </div>
+                        <div className="flex flex-col items-center mb-14">
+                            <span className="text-2xl mb-5">SITE</span>
+                            <ul className="flex justify-between items-center w-32">
+                                <li className="text-gray-400 hover:border-b-2 border-purple-400">create</li>
+                                <li className="text-gray-400 hover:border-b-2 border-purple-400">update</li>
+                            </ul>
+                        </div>
+                    </div>
                 :
-                null
+                    null
             }
         </>
     );
