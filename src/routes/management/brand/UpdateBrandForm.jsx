5747d886df0639efd3bcb155280376f1ca51bb2d
ohnas <ohnas12@gmail.com>
2023-02-27 13:41:48 +0900

    52. UpdateBrandForm.jsx

--- src/routes/management/brand/UpdateBrandForm.jsx
+++ src/routes/management/brand/UpdateBrandForm.jsx
@@ -10,19 +10,13 @@ function UpdateBrandForm() {
     const navigate = useNavigate();
     let {brandPk} = useParams();
     const [brandDetail, setBrandDetail] = useState({});
-    const [updateForm, setUpdateForm] = useState("");
+    const [userList, setUserList] = useState([]);
     const { register, handleSubmit } = useForm();
     function goHome() {
         if(user.is_staff === false) {
             alert("이용 할 수 없는 페이지 입니다");
             return navigate("/");
         }
-    }
-    function updateBtn(event) {
-        setUpdateForm(event.target.value);
-    }
-    function cancleBtn(event) {
-        setUpdateForm(event.target.value);
     }
     async function getBrandDetail() {
         let response = await fetch(`${baseUrl}/brands/update/${brandPk}`, {
@@ -35,6 +29,17 @@ function UpdateBrandForm() {
         let data = await response.json();
         setBrandDetail(data);
     }
+    async function handleUserList() {
+        let response = await fetch(`${baseUrl}/brands/create`, {
+            method : "GET",
+            credentials: "include",
+            headers : {
+                'Content-Type': 'application/json',
+            },
+        });
+        let data = await response.json();
+        setUserList(data);
+    }
     async function onSubmit(updateData) {
         if(updateData.name === "") {
             delete updateData.name;
@@ -42,20 +47,21 @@ function UpdateBrandForm() {
         if(updateData.description === "") {
             delete updateData.description;
         }
-        console.log(updateData);
-        // let csrftoken = getCookie('csrftoken');
-        // let response = await fetch(`${baseUrl}/brands/update/${brandPk}` , {
-        //     method : "PUT",
-        //     credentials: "include",
-        //     headers : {
-        //         'Content-Type': 'application/json',
-        //         "X-CSRFToken": csrftoken,
-        //     },
-        //     body : JSON.stringify(updateData),
-        // });
-        // let data = await response.json();
-        // setBrandDetail(data);
-        // setUpdateForm("");
+        if(updateData.user === "") {
+            delete updateData.user;
+        }
+        let csrftoken = getCookie('csrftoken');
+        let response = await fetch(`${baseUrl}/brands/update/${brandPk}` , {
+            method : "PUT",
+            credentials: "include",
+            headers : {
+                'Content-Type': 'application/json',
+                "X-CSRFToken": csrftoken,
+            },
+            body : JSON.stringify(updateData),
+        });
+        let data = await response.json();
+        setBrandDetail(data);
     }
     useEffect(() => {
         goHome();
@@ -63,62 +69,39 @@ function UpdateBrandForm() {
     useEffect(() => {
         getBrandDetail();
     }, []);
+    useEffect(() => {
+        handleUserList();
+    }, []);
     return (
-        <div className="mt-24">
+        <div className="mt-24 flex justify-center items-center">
             {user.is_staff ?
                     <>
                         {Object.keys(brandDetail).length === 0 ? 
                                 null
                             :
-                                <div className="flex justify-around">
-                                    <div className="border-2 rounded-md w-72 border-gray-200 text-center relative shadow-md">
-                                        <div className="absolute left-2 -top-4 bg-white text-xl">
-                                            <span>NAME</span>
-                                        </div>
-                                        <div className="absolute right-2 -bottom-3 bg-white">
-                                            {updateForm === "" ? 
-                                                    <button onClick={updateBtn} value="brandName" className="text-gray-400 mt-2 hover:text-purple-400">update</button>
-                                                :
-                                                    <button onClick={cancleBtn} value="" className="text-gray-400 mt-2 hover:text-purple-400">cancle</button>
-                                            }
-                                        </div>
-                                        <div className="flex flex-col justify-center items-center mt-5 mb-5">
-                                            <span>{brandDetail.name}</span>
-                                            {updateForm === "" ? 
-                                                    null
-                                                :
-                                                    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col mt-5">
-                                                        <input {...register("name")} id="name" type="text" className="border-2 rounded-md w-52 border-gray-200" />
-                                                        <button className="text-violet-600">save</button>
-                                                    </form>
-                                            }
-                                        </div>
+                                <>
+                                    <div className="flex flex-col border-2 w-80 h-80 justify-center items-center rounded-md shadow-md">
+                                        <label htmlFor="name">NAME</label>
+                                        <span id="name" className="border-2 rounded-md w-72 border-gray-200 mb-10 text-center">{brandDetail.name}</span>
+                                        <label htmlFor="description">DESCRIPTION</label>
+                                        <span id="description" className="border-2 rounded-md w-72 border-gray-200 mb-10 text-center">{brandDetail.description}</span>
+                                        <label htmlFor="user">BM</label>
+                                        <span id="user" className="border-2 rounded-md w-72 border-gray-200 mb-10 text-center">{brandDetail.user.name}</span>
                                     </div>
-                                    <div className="border-2 rounded-md w-72 border-gray-200 text-center relative shadow-md">
-                                        <div className="absolute left-2 -top-4 bg-white text-xl">
-                                            <span>DESCRIPTION</span>
-                                        </div>
-                                        <div className="absolute right-2 -bottom-3 bg-white">
-                                            {updateForm === "" ? 
-                                                    <button onClick={updateBtn} value="brandDescription" className="text-gray-400 mt-2 hover:text-purple-400">update</button>
-                                                :
-                                                    <button onClick={cancleBtn} value="" className="text-gray-400 mt-2 hover:text-purple-400">cancle</button>
-                                            }
-                                        </div>
-                                        <div className="flex flex-col justify-center items-center mt-5 mb-5">
-                                            <span>{brandDetail.description}</span>
-                                            {updateForm === "" ? 
-                                                    null
-                                                :
-                                                    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col mt-5">
-                                                        <input {...register("description")} id="description" type="text" className="border-2 rounded-md w-52 border-gray-200" />
-                                                        <button className="text-violet-600">save</button>
-                                                    </form>
-                                            }
-                                        </div>
-                                    </div>
-                                    <span>{brandDetail.user.name}</span>
-                                </div>
+                                    <form onSubmit={handleSubmit(onSubmit)} className="flex flex-col justify-center items-center border-2 w-80 h-80 rounded-md shadow-md">
+                                        <label htmlFor="name">NAME</label>
+                                        <input {...register("name")} id="name" type="text" className="border-2 rounded-md w-72 border-gray-200 mb-10" />
+                                        <label htmlFor="description">DESCRIPTION</label>
+                                        <input {...register("description")} id="description" type="text" className="border-2 rounded-md w-72 border-gray-200 mb-10" />
+                                        <select {...register("user")} id="user" className="border-2 rounded-md w-72 border-gray-200 mb-10 text-center">
+                                            <option value="">BM</option>
+                                            {userList.map((user) => 
+                                                <option key={user.pk} value={user.pk}>{user.name}</option>
+                                            )}
+                                        </select>
+                                        <button className="w-56 h-12 hover:border-b-2 border-purple-500">UPDATE</button>
+                                    </form>
+                                </>
                         }
                     </>
                 :
