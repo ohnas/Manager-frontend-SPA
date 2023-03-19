4d04d2184a6aba0c9419cba2e6be2a009ad20a88
ohnas <ohnas12@gmail.com>
2023-03-19 14:06:25 +0900

    getcookie()

--- src/api.js
+++ src/api.js
@@ -1,9 +1,24 @@
 export const baseUrl = "https://backend.managertestapp.com/api/v1"
 
 
+// export function getCookie(name) {
+//     let matches = document.cookie.match(new RegExp(
+//         "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
+//     ));
+//     return matches ? decodeURIComponent(matches[1]) : undefined;
+// }
+
 export function getCookie(name) {
-    let matches = document.cookie.match(new RegExp(
-        "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-    ));
-    return matches ? decodeURIComponent(matches[1]) : undefined;
+    const cookieValue = document.cookie.split(';')
+        .map(cookie => cookie.trim())
+        .find(cookie => cookie.startsWith(`${name}=`));
+
+    if (!cookieValue) {
+        return undefined;
+    }
+
+    return decodeURIComponent(cookieValue.split('=')[1]);
 }
+
+console.log(getCookie('csrftoken'));
+  
