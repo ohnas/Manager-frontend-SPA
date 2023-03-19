51c7febd0c5329ee61f5161b5b937d8bd48dc93e
ohnas <ohnas12@gmail.com>
2023-03-19 16:02:38 +0900

    getCookie

--- src/api.js
+++ src/api.js
@@ -1,18 +1,10 @@
 export const baseUrl = "https://backend.managertestapp.com/api/v1"
 
-
-// export function getCookie(name) {
-//     let matches = document.cookie.match(new RegExp(
-//         "(?:^|; )" + name.replace(/([\.$?*|{}\(\)\[\]\\\/\+^])/g, '\\$1') + "=([^;]*)"
-//     ));
-//     return matches ? decodeURIComponent(matches[1]) : undefined;
-// }
-
 export function getCookie(name) {
     const cookieValue = document.cookie.split(';')
         .map(cookie => cookie.trim())
         .find(cookie => cookie.startsWith(`${name}=`));
-    console.log(cookieValue);
+        
     if (!cookieValue) {
         return undefined;
     }
