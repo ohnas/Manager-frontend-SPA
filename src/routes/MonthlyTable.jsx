f3b60ab76fc2feea9c8d2bbd49deb732e079bb13
ohnas <ohnas12@gmail.com>
2023-05-30 13:17:08 +0900

    change style

--- src/routes/MonthlyTable.jsx
+++ src/routes/MonthlyTable.jsx
@@ -138,7 +138,7 @@ function MonthlyTable() {
                     {noData ? 
                         <div className="flex justify-center items-center h-screen">
                             <div className="flex justify-center items-center">
-                                <span className="text-gray-400">No data.</span>
+                                <span className="text-red-500">No data.</span>
                             </div>
                         </div>
                         :
