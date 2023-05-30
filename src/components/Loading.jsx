fd676c93b4ecc7e1807820adfcc53b5d95d2a07f
ohnas <ohnas12@gmail.com>
2023-05-30 13:27:50 +0900

    Loading.jsx

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,8 +1,8 @@
 function Loading() {
     return (
         <div className="flex justify-center items-center h-screen">
-            <div className="flex justify-center items-center border-4 rounded-full h-48 w-48 animate-pulse">
-                <span className="text-gray-400">Processing...</span>
+            <div className="flex justify-center items-center border-4 rounded-full h-48 w-48 animate-pulse border-green-200">
+                <span className="text-green-500">Processing...</span>
             </div>
         </div>
     );                     
